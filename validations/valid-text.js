b995721589fafdd886e4fd8cf024023db5592131
Philippe Fonzin <pfonzin@gmail.com>
2020-03-15 17:32:32 -0400

    Add user validations

---
+++ validations/valid-text.js
@@ -0,0 +1,6 @@
+// ckecks wheather a given string consists of valid input 
+const validText = str => {
+  return typeof str === "string" && str.trim().length > 0;
+};
+
+module.exports = validText; 
