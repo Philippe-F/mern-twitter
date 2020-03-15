99596de496c4f9ee863883e1a4b795ab3a6b77b3
Philippe Fonzin <pfonzin@gmail.com>
2020-03-15 16:17:09 -0400

    Access chrome dev tools on node

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 const express = require("express");
-const app = express(); // This creates a new Express server
-const db = require("./config/keys").mongoURI;
-const mongoose = require("mongoose");
+const app = express(); // this creates a new Express server / app object
+const db = require("./config/keys").mongoURI; 
+// gets URI (Uniform Resource Identifier) from keys.js 
+const mongoose = require("mongoose"); // imports mongoose dependency 
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 
-const bodyParser = require('body-parser'); //parses the JSON we send to the frontend 
+const bodyParser = require('body-parser'); // parses the JSON we send to the frontend 
 
 app.get("/", (req, res) => res.send("Hello Young World"));
-// setups a route so that we can render some information on our page
+// Puts a new route onto the app object to listen for incoming (GET, PATCH, etc) requests
+// "/" is the route (root). req is a request from the server (postId, etc)
+// res is sent to the frontend
 
-mongoose
+mongoose // connect mongoose to the db
   .connect(db, { useNewUrlParser: true })
+  // first argument is te URI second argument is a configuration object 
   .then(() => console.log("Connected to MongoDB successfully"))
+  // .connect returns a promise. dispatch a message if connected 
   .catch(err => console.log(err));
+  // dispatch a message if error
 
 
 const port = process.env.PORT || 5000; // tells our app which port to run on
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+// heroku might want us to listen on a different port so (process.env.PORT) 
+// grabs a port variable out of the environment variables on heroku
+
+// (app.listen) first argument is the port were gonna listen on, the second
+// arument is the a callback to be invoked once it has started listening. 
+
 // tells Express to start a socket and listen for connections on the path and
 // will log a success message to the console when our server is running 
 // successfully
@@ -29,4 +41,4 @@ app.use("/api/tweets", tweets);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-//middleware for the body parser 
+// middleware for the body parser 
